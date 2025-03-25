11aea98e73c986e33a4172801338800ef8786b4f
karol5108 <karol5108@gmail.com>
2025-03-25 18:36:11 +0100

    first commit, microservices project structure, connection to h2 db

---
+++ campaign-service/src/test/java/pl/edu/wszib/campaign_service/CampaignServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.campaign_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
